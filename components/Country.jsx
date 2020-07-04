881f362f8608ef5c188b58659693b6d71ed929c7
Janis Basis Basovs <janis_basis@hotmail.com>
2020-07-04 20:11:22 +0300

    select box integrated

--- components/Country.jsx
+++ components/Country.jsx
@@ -1,13 +1,98 @@
-const { render } = require("react-dom");
+import styled from "styled-components"
+import Head from "next/head"
 
 class Country extends React.Component {
-    state = {
-        country: "latvia",
-    };
+  state = {
+    country: "usa",
+  }
 
-    render() {
-        return <div>Country {this.props.data}</div>;
+  render() {
+    // const dataObj = JSON.parse(this.props.data.response)
+    const data = this.props.data
+
+    // console.log("Before data")
+    // console.log(data[67].country) /* Latvia */
+    // console.log(data[14].country) /* Sweden */
+    // console.log(data[3].country) /* USA */
+
+    let statistics = ""
+    if (this.state.country === "latvia") {
+      statistics = (
+        <div>
+          <h1>Covid - {data[67].country}</h1>
+          <h2>Atjaunots - {data[67].day}</h2>
+          <h3>Līdz šim apstiprināti gadījumi</h3>
+          <Code>{data[67].cases.total}</Code>
+          <h3>Nomiruši</h3>
+          <Code>{data[67].deaths.total}</Code>
+          <h3>Izveseļojušies</h3>
+          <Code>{data[67].cases.recovered}</Code>
+          <h3>Vēl slimo</h3>
+          <Code>{data[67].cases.active}</Code>
+        </div>
+      )
+    } else if (this.state.country === "sweden") {
+      statistics = (
+        <div>
+          <h1>Covid - {data[14].country}</h1>
+          <h2>Atjaunots - {data[14].day}</h2>
+          <h3>Līdz šim apstiprināti gadījumi</h3>
+          <Code>{data[14].cases.total}</Code>
+          <h3>Nomiruši</h3>
+          <Code>{data[14].deaths.total}</Code>
+          <h3>Izveseļojušies</h3>
+          <Code>{data[14].cases.recovered}</Code>
+          <h3>Vēl slimo</h3>
+          <Code>{data[14].cases.active}</Code>
+        </div>
+      )
+    } else if (this.state.country === "usa") {
+      statistics = (
+        <div>
+          <h1>Covid - {data[3].country}</h1>
+          <h2>Atjaunots - {data[3].day}</h2>
+          <h3>Līdz šim apstiprināti gadījumi</h3>
+          <Code>{data[3].cases.total}</Code>
+          <h3>Nomiruši</h3>
+          <Code>{data[3].deaths.total}</Code>
+          <h3>Izveseļojušies</h3>
+          <Code>{data[3].cases.recovered}</Code>
+          <h3>Vēl slimo</h3>
+          <Code>{data[3].cases.active}</Code>
+        </div>
+      )
     }
+
+    return (
+      <div state={this.state}>
+        <Head>
+          <title>Covid-Test</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <select
+          name="pickCountry"
+          id="pickCountry"
+          onChange={(e) => {
+            this.setState({ country: e.target.value }, console.log(e.target.value))
+          }}
+        >
+          <option value="latvia">Latvia</option>
+          <option value="sweden">Sweden</option>
+          <option value="usa">USA</option>
+        </select>
+        {statistics}
+      </div>
+    )
+  }
 }
 
-export default Country;
+export default Country
+
+const Code = styled.div`
+  background: #fafafa;
+  border-radius: 5px;
+  padding: 0.75rem;
+  font-size: 1.5rem;
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace;
+`
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,131 +1,75 @@
-import Head from "next/head";
-import styled from "styled-components";
-import useSWR from "swr";
+import Head from "next/head"
+import styled from "styled-components"
+import useSWR from "swr"
 
-import Country from "../components/Country";
+import Country from "../components/Country"
 
-let url = "https://covid-193.p.rapidapi.com/statistics?country=latvia";
+let url = "https://covid-193.p.rapidapi.com/statistics"
 
 // Fetching data from a API - with useSWR
 const fetcher = (...args) =>
-    fetch(...args, {
-        method: "GET",
-        headers: {
-            "x-rapidapi-host": "covid-193.p.rapidapi.com",
-            "x-rapidapi-key":
-                "86bf3463b7mshd48a8642e3877b1p14f8c5jsn8fe7a3629fb2",
-        },
-    }).then(res => res.json());
+  fetch(...args, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "covid-193.p.rapidapi.com",
+      "x-rapidapi-key": "86bf3463b7mshd48a8642e3877b1p14f8c5jsn8fe7a3629fb2",
+    },
+  }).then((res) => res.json())
 
 // Components
 const Index = () => {
-    const { data, error } = useSWR(url, fetcher);
-    if (error) return <div>failed to load</div>;
-    if (!data)
-        return (
-            <Wrapper>
-                <div>loading...</div>
-            </Wrapper>
-        );
-    // console.log(props.data); <<<<<--------------------------------------------------------------------------- console.log()
-    console.log(data.response[0]);
-    console.log("janis");
+  const { data, error } = useSWR(url, fetcher)
+  if (error) return <div>failed to load</div>
+  if (!data)
     return (
-        <Wrapper>
-            <Head>
-                <title>Covid-{data.response[0].country}</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+      <Wrapper>
+        <div>loading...</div>
+      </Wrapper>
+    )
 
-            <Main>
-                <Country data={data.response[0].country} />
-                <h1>Covid - {data.response[0].country}</h1>
-                <h2>Atjaunots - {data.day}</h2>
-                <br />
-                {/* <select
-                    name="pickCountry"
-                    id="pickCountry"
-                    onChange={changeCountry}
-                >
-                    <option value="latvia">Latvia</option>
-                    <option value="sweden">Sweden</option>
-                    <option value="usa">USA</option>
-                </select> */}
-                <h3>Līdz šim apstiprināti gadījumi</h3>
-                <Code>{data.response[0].cases.total}</Code>
-                <h3>Nomiruši</h3>
-                <Code>{data.response[0].deaths.total}</Code>
-                <h3>Izveseļojušies</h3>
-                <Code>{data.response[0].cases.recovered}</Code>
-                <h3>Vēl slimo</h3>
-                <Code>{data.response[0].cases.active}</Code>
-            </Main>
+  //   console.log(data.response[67]) /* Latvia */
+  //   console.log(data.response[14]) /* Sweden */
+  //   console.log(data.response[3]) /* USA */
+  //   console.log("janis")
 
-            <style jsx global>{`
-                html,
-                body {
-                    padding: 0;
-                    margin: 0;
-                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
-                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                        Helvetica Neue, sans-serif;
-                }
+  return (
+    <Wrapper>
+      <Head>
+        <title>Covid-{data.response[67].country}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-                * {
-                    box-sizing: border-box;
-                }
-            `}</style>
-        </Wrapper>
-    );
-};
+      <Main>
+        <Country data={data.response} />
+      </Main>
 
-export default Index;
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </Wrapper>
+  )
+}
+
+export default Index
 
 const Wrapper = styled.div`
-    min-height: 100vh;
-    padding: 0 0.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 const Main = styled.main`
-    text-align: center;
-`;
-const Code = styled.div`
-    background: #fafafa;
-    border-radius: 5px;
-    padding: 0.75rem;
-    font-size: 1.5rem;
-    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-        DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-`;
-
-// export async function getServerSideProps() {
-//     const res = await fetch(url, {
-//         method: "GET",
-//         headers: {
-//             "x-rapidapi-host": "covid-193.p.rapidapi.com",
-//             "x-rapidapi-key":
-//                 "86bf3463b7mshd48a8642e3877b1p14f8c5jsn8fe7a3629fb2",
-//         },
-//     });
-//     const data = await res.json();
-
-//     return {
-//         props: data,
-//     };
-// }
-
-// Logic for Select box
-// let choosenCountry = "latvia";
-// let url =
-//     "https://covid-193.p.rapidapi.com/statistics?country=" + choosenCountry;
-// const changeCountry = e => {
-//     const myValue = e.target.value;
-//     console.log("Country picked " + myValue);
-//     choosenCountry = myValue;
-//     url =
-//         "https://covid-193.p.rapidapi.com/statistics?country=" + choosenCountry;
-//     console.log("Your New url " + url);
-// };
+  text-align: center;
+`
