cdb97ed68d2d26542e4224b6d6a620fa6ca47b56
Janis Basis Basovs <janis_basis@hotmail.com>
2020-12-05 12:42:18 +0200

    api update

--- components/Country.jsx
+++ components/Country.jsx
@@ -1,106 +1,107 @@
-import styled from "styled-components";
-import Head from "next/head";
+import styled from "styled-components"
+import Head from "next/head"
 
 class Country extends React.Component {
   state = {
     country: "latvia",
-  };
+  }
 
   render() {
     // const dataObj = JSON.parse(this.props.data.response)
-    const data = this.props.data;
+    const data = this.props.data
 
     // console.log("Before data")
-    // console.log(data[67].country) /* Latvia */
-    // console.log(data[14].country) /* Sweden */
-    // console.log(data[3].country) /* USA */
+    // console.log(data[110].country) /* Latvia */
+    // console.log(data[26].country) /* Sweden */
+    // console.log(data[1].country) /* USA */
+    // console.log(data[19].country) /* Visaa pasaulee */
 
-    let statistics = "";
+    let statistics = ""
     if (this.state.country === "latvia") {
       statistics = (
         <div>
           <Head>
-            <title>Covid19 - {data[67].country}</title>
+            <title>Covid19 - {data[110].country}</title>
           </Head>
-          <h1>Covid19 - {data[67].country}</h1>
-          <p>Atjaunots - {data[67].day}. Statistika atjaunojas 1x dienā.</p>
+          <h1>Covid19 - {data[110].country}</h1>
+          <p>Atjaunots - {data[110].day}. Statistika atjaunojas 1x dienā.</p>
           <article></article>
 
           <Article>
             <h2>Līdz šim apstiprināti gadījumi</h2>
-            <Code>{data[67].cases.total}</Code>
+            <Code>{data[110].cases.total}</Code>
           </Article>
 
           <Article>
             <h2>Miruši</h2>
-            <Code>{data[67].deaths.total}</Code>
+            <Code>{data[110].deaths.total}</Code>
           </Article>
 
           <Article>
             <h2>Izveseļojušies</h2>
-            <Code>{data[67].cases.recovered}</Code>
+            <Code>{data[110].cases.recovered}</Code>
           </Article>
 
           <Article>
             <h2>Vēl slimo</h2>
-            <Code>{data[67].cases.active}</Code>
+            <Code>{data[110].cases.active}</Code>
           </Article>
         </div>
-      );
+      )
     } else if (this.state.country === "sweden") {
       statistics = (
         <div>
           <Head>
-            <title>Covid19 - {data[14].country}</title>
+            <title>Covid19 - {data[26].country}</title>
           </Head>
-          <h1>Covid19 - {data[14].country}</h1>
-          <p>Atjaunots - {data[14].day}. Statistika atjaunojas 1x dienā.</p>
+          <h1>Covid19 - {data[26].country}</h1>
+          <p>Atjaunots - {data[26].day}. Statistika atjaunojas 1x dienā.</p>
 
           <Article>
             <h2>Līdz šim apstiprināti gadījumi</h2>
-            <Code>{data[14].cases.total}</Code>
+            <Code>{data[26].cases.total}</Code>
           </Article>
           <Article>
             <h2>Miruši</h2>
-            <Code>{data[14].deaths.total}</Code>
+            <Code>{data[26].deaths.total}</Code>
           </Article>
           <Article>
             <h2>Izveseļojušies</h2>
-            <Code>{data[14].cases.recovered}</Code>
+            <Code>{data[26].cases.recovered}</Code>
           </Article>
           <Article>
             <h2>Vēl slimo</h2>
-            <Code>{data[14].cases.active}</Code>
+            <Code>{data[26].cases.active}</Code>
           </Article>
         </div>
-      );
+      )
     } else if (this.state.country === "usa") {
       statistics = (
         <div>
           <Head>
-            <title>Covid19 - {data[3].country}</title>
+            <title>Covid19 - {data[1].country}</title>
           </Head>
-          <h1>Covid19 - {data[3].country}</h1>
-          <p>Atjaunots - {data[3].day}. Statistika atjaunojas 1x dienā.</p>
+          <h1>Covid19 - {data[1].country}</h1>
+          <p>Atjaunots - {data[1].day}. Statistika atjaunojas 1x dienā.</p>
 
           <Article>
             <h2>Līdz šim apstiprināti gadījumi</h2>
-            <Code>{data[3].cases.total}</Code>
+            <Code>{data[1].cases.total}</Code>
           </Article>
           <Article>
             <h2>Miruši</h2>
-            <Code>{data[3].deaths.total}</Code>
+            <Code>{data[1].deaths.total}</Code>
           </Article>
           <Article>
             <h2>Izveseļojušies</h2>
-            <Code>{data[3].cases.recovered}</Code>
+            <Code>{data[1].cases.recovered}</Code>
           </Article>
           <Article>
             <h2>Vēl slimo</h2>
-            <Code>{data[3].cases.active}</Code>
+            <Code>{data[1].cases.active}</Code>
           </Article>
         </div>
-      );
+      )
     }
 
     return (
@@ -110,11 +111,11 @@ class Country extends React.Component {
         <Select
           name="pickCountry"
           id="pickCountry"
-          onChange={e => {
+          onChange={(e) => {
             this.setState(
               { country: e.target.value }
               // console.log(e.target.value)
-            );
+            )
           }}
         >
           <option value="latvia">Latvia</option>
@@ -122,16 +123,16 @@ class Country extends React.Component {
           <option value="usa">USA</option>
         </Select>
       </Statistics>
-    );
+    )
   }
 }
 
-export default Country;
+export default Country
 const Statistics = styled.div`
   p {
     color: #4b4b4b;
   }
-`;
+`
 const Article = styled.article`
   display: flex;
   align-items: center;
@@ -141,7 +142,7 @@ const Article = styled.article`
     font-size: 1.2rem;
     font-weight: 500;
   }
-`;
+`
 
 const Code = styled.div`
   display: inline-block;
@@ -151,10 +152,10 @@ const Code = styled.div`
   font-size: 1.3rem;
   font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
     Bitstream Vera Sans Mono, Courier New, monospace;
-`;
+`
 const Label = styled.label`
   font-weight: 500;
-`;
+`
 const Select = styled.select`
   outline: none;
   inline-size: 100%;
@@ -173,4 +174,4 @@ const Select = styled.select`
     font-size: 1.2rem;
     font-weight: bold;
   }
-`;
+`
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import styled from "styled-components";
-import useSWR from "swr";
+import Head from "next/head"
+import styled from "styled-components"
+import useSWR from "swr"
 
-import Country from "../components/Country";
+import Country from "../components/Country"
 
-let url = "https://covid-193.p.rapidapi.com/statistics";
+let url = "https://covid-193.p.rapidapi.com/statistics"
 
 // Fetching data from a API - with useSWR
 const fetcher = (...args) =>
@@ -14,23 +14,24 @@ const fetcher = (...args) =>
       "x-rapidapi-host": "covid-193.p.rapidapi.com",
       "x-rapidapi-key": "86bf3463b7mshd48a8642e3877b1p14f8c5jsn8fe7a3629fb2",
     },
-  }).then(res => res.json());
+  }).then((res) => res.json())
 
 // Components
 const Index = () => {
-  const { data, error } = useSWR(url, fetcher);
-  if (error) return <div>failed to load</div>;
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>failed to load</div>
   if (!data)
     return (
       <Wrapper>
         <div>loading...</div>
       </Wrapper>
-    );
+    )
 
   //   console.log(data.response[67]) /* Latvia */
-  console.log(data.response[14]); /* Sweden */
+  // console.log(data.response[14]); /* Sweden */
   //   console.log(data.response[3]) /* USA */
-  //   console.log("janis")
+  console.log(data.response) /* USA */
 
   return (
     <Wrapper>
@@ -69,10 +70,10 @@ const Index = () => {
         }
       `}</style>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -81,5 +82,5 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-const Main = styled.main``;
+`
+const Main = styled.main``
